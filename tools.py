d4699b7bf73b86b09e9f909d906a19baaa0fef1b
José Alexandre Costa de Lacerda <jcl@certi.org.br>
2020-03-29 20:34:42 -0300

    Add scraaping tools for SC govern bulletins

--- tools.py
+++ tools.py
@@ -31,34 +31,49 @@ def load_dataset(cities=None):
         return df[df['city'].isin(cities)]
     
 
-def scrap_paragraph(paragraph, date=None, outside=0):
+def scrap_paragraph(paragraph, date=None, pattern=1, outside=0):
     """
     Extract SC cities total cases from daily bulletins.
     Is date is not given, gets the actual day.
+    
+    Pattern=1 (default) is for texts like this:
+    '...Blumenau (8), Usussanga (12), ...'
+    
+    Pattern=2 is for texts like this:
+    '
+    Joinville – 9
+    Lages – 1
+    Laguna – 1
+    '
+    
     """
     if date is None:
         date = datetime.today().strftime('%Y-%m-%d')
     
-    regexp = '([,e: ]+ )([\w ]+) (\([\d]+\))'
+    if pattern == 1:
+        regexp = '(?:[,e: ]+ )([\w ]+) (?:\()([\d]+)'
+    elif pattern == 2:
+        regexp = '(?:\\n)([\w ]+)(?: – )([\d]+)'
+    
     occurrences = re.findall(regexp, paragraph)
     
     data = dict()
     data['date'] = [date]
-    for city in occurrences:
-        data[city[1]] = [int(city[2][1:-1])]
+    for o in occurrences:
+        data[o[0]] = [int(o[1])]
     
     data['Fora de SC'] = [outside]
     
     return pd.DataFrame(data)
 
 
-def update_tabular(paragraph, date=None, outside=0):
+def update_tabular(paragraph, pattern=1, date=None, outside=0):
     """
     Updates the saved tabular dataset with numbers
     scrapped from SC government bulletin.
     """
     old_df = pd.read_csv('tabular.csv')
-    new_df = scrap_paragraph(paragraph, date, outside)
+    new_df = scrap_paragraph(paragraph, date, pattern, outside)
     
     return (
         pd
