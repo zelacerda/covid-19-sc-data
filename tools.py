82f0587a8c5f3693c398ba2544b29497e93cc0b3
José Alexandre Costa de Lacerda <jcl@certi.org.br>
2020-04-10 17:28:02 -0300

    Add regex pattern autodetection

--- tools.py
+++ tools.py
@@ -3,6 +3,32 @@ import re
 import pandas as pd
 
 
+"""
+regex_pattern stores different patterns to find data from text.
+
+Pattern 1 is for texts like this:
+    
+...Blumenau (8), Usussanga (12), ...
+
+Pattern 2 is for texts like this:
+
+Joinville – 9
+Lages – 1
+Laguna – 1
+
+Pattern 3 is for texts like this:
+
+Joinville 9
+Lages 1
+Laguna 1 
+"""
+regex_pattern = {
+    1: '(?:[,e: ]+ )([\w ]+) (?:\()([\d]+)',
+    2: '(?:\\n)([\w ]+)(?: [–-]? )([\d]+)',
+    3: '(?:\\n)([\w ]+)(?: )([\d]+)'
+}
+
+
 def update_data():
     """
     Updates dataset from manual input data
@@ -31,49 +57,48 @@ def load_dataset(cities=None):
         return df[df['city'].isin(cities)]
     
 
-def scrap_paragraph(paragraph, date=None, pattern=1):
+def find_pattern(text):
+    max_value = 0
+    max_item = 1
+    for item, pattern in regex_pattern.items():
+        match_occurences = len(re.findall(pattern, text))
+        if match_occurences > max_value:
+            max_value = match_occurences
+            max_item = item
+    return max_item
+    
+
+def scrap_text(text, pattern=None):
     """
     Extract SC cities total cases from daily bulletins.
-    Is date is not given, gets the actual day.
+    Is date is not given, gets the actual day.  
+    """  
+    if pattern is None:
+        regexp = regex_pattern[find_pattern(text)]
+    else:
+        regexp = regex_pattern[pattern]
     
-    Pattern=1 (default) is for texts like this:
-    '...Blumenau (8), Usussanga (12), ...'
-    
-    Pattern=2 is for texts like this:
-    '
-    Joinville – 9
-    Lages – 1
-    Laguna – 1
-    '
-    
-    """
-    if date is None:
-        date = datetime.today().strftime('%Y-%m-%d')
-    
-    if pattern == 1:
-        regexp = '(?:[,e: ]+ )([\w ]+) (?:\()([\d]+)'
-    elif pattern == 2:
-        regexp = '(?:\\n)([\w ]+)(?: [–-]? )([\d]+)'
-    elif pattern == 3:
-        regexp = '(?:\\n)([\w ]+)(?: )([\d]+)'
-    
-    occurrences = re.findall(regexp, paragraph)
+    occurrences = re.findall(regexp, text)
     
     data = dict()
-    data['date'] = [date]
     for o in occurrences:
         data[o[0]] = [int(o[1])]
   
-    return pd.DataFrame(data)
+    return data
 
 
-def update_tabular(paragraph, pattern=1, date=None):
+def update_tabular(text, date=None, pattern=None):
     """
     Updates the saved tabular dataset with numbers
     scrapped from SC government bulletin.
     """
+    if date is None:
+        date = datetime.today().strftime('%Y-%m-%d')
+        
     old_df = pd.read_csv('tabular.csv')
-    new_df = scrap_paragraph(paragraph, date, pattern)
+    data = scrap_text(text, pattern)
+    new_df = pd.DataFrame(data)
+    new_df['date'] = date
     
     return (
         pd
